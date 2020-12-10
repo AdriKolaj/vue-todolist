7f7e1164cb302f7c210a5eda8971fa049f0ea59d
adri <adri.kolaj@gmail.com>
2020-12-10 15:35:35 +0100

    completato

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // - creare una lista di cose da fare e mostrarla in pagina con Vue;
 // - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
 // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+// - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
 
 var app = new Vue({
 
@@ -14,16 +15,21 @@ var app = new Vue({
       "Completare l'esercitazione"
     ],
     newToDo: "",
+    alertMessage: "",
+    isHovering: false
   },
 
   methods: {
     addNew: function() {
+      if (this.newToDo != "") {
       this.toDoList.push(this.newToDo),
-      this.newToDo = ""
+      this.newToDo = "",
+      this.alertMessage = ""
+    } else this.alertMessage = "Inserisci un'attività valida!"
     },
     deleteX: function(index) {
       this.toDoList.splice(index, 1)
-    }
+    },
   }
 
 });
